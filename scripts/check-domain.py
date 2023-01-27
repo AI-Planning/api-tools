4a63f37a5dcc451e082c9972908bf8c0cfb94e42
baonny <baonnyhui11@gmail.com>
2023-01-27 14:24:35 -0500

    Update check-domain.py

--- scripts/check-domain.py
+++ scripts/check-domain.py
@@ -1,4 +1,19 @@
 import os, sys
 
+current = os.path.dirname(os.path.realpath(__file__))
+parent_directory = os.path.dirname(current)
+sys.path.append(parent_directory)
+
+import planning_domains_api as api
+
 if __name__ == "__main__":
-    os.system("python ../planning.domains.py find domains {}".format(str(sys.argv[1])))
+    user_input = 0
+
+    # Using system arguments 
+    try:
+        user_input = int(input("What domain would you like to check: "))
+        domain = api.get_domain(user_input)
+    except:
+        print("Input is not a number")
+    
+    print(domain)
